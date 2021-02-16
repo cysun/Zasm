590ea4827bfe45afcb7590474229a360a875bc92
Chengyu Sun <csun@calstatela.edu>
2021-02-16 15:45:27 -0800

    Added ListCultures

---
+++ ListCultures/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+namespace ListCultures
+{
+    class Program
+    {
+        static void Main()
+        {
+            var cultures = CultureInfo.GetCultures(CultureTypes.AllCultures);
+            foreach (var culture in cultures)
+                Console.WriteLine($"{culture.Name} {culture.EnglishName}");
+        }
+    }
+}
